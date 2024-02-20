3ee8255cf66c3c221c59263b7d2e4672e3ab16f3
SofiaValadares <svc3@cesar.school>
2024-02-20 09:09:56 +0000

    codigo professor

---
+++ sempapi.cpp
@@ -0,0 +1,196 @@
+#include <stdio.h>
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+#include <cstdlib>
+
+using namespace std;
+
+#define SYSTEMTIME clock_t
+
+ 
+void OnMult(int m_ar, int m_br) 
+{
+	
+	SYSTEMTIME Time1, Time2;
+	
+	char st[100];
+	double temp;
+	int i, j, k;
+
+	double *pha, *phb, *phc;
+	
+
+		
+    pha = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phb = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phc = (double *)malloc((m_ar * m_ar) * sizeof(double));
+
+	for(i=0; i<m_ar; i++)
+		for(j=0; j<m_ar; j++)
+			pha[i*m_ar + j] = (double)1.0;
+
+
+
+	for(i=0; i<m_br; i++)
+		for(j=0; j<m_br; j++)
+			phb[i*m_br + j] = (double)(i+1);
+
+
+
+    Time1 = clock();
+
+	for(i=0; i<m_ar; i++)
+	{	printf("\nmatris:\n");
+		for( j=0; j<m_br; j++)
+		{	temp = 0;
+			for( k=0; k<m_ar; k++)
+			{	
+				temp += pha[i*m_ar+k] * phb[k*m_br+j];
+				printf("%f ", temp);
+			}
+			printf("%f ", temp);
+			printf("\n");
+			phc[i*m_ar+j]=temp;
+
+		}
+	}
+
+
+    Time2 = clock();
+	sprintf(st, "Time: %3.3f seconds\n", (double)(Time2 - Time1) / CLOCKS_PER_SEC);
+	cout << st;
+
+	// display 10 elements of the result matrix tto verify correctness
+	cout << "Result matrix: " << endl;
+	for(i=0; i<1; i++)
+	{	for(j=0; j<min(10,m_br); j++)
+			cout << phc[j] << " ";
+	}
+	cout << endl;
+
+    free(pha);
+    free(phb);
+    free(phc);
+	
+	
+}
+
+// add code here for line x line matriz multiplication
+void OnMultLine(int m_ar, int m_br)
+{
+    	
+	SYSTEMTIME Time1, Time2;
+	
+	char st[100];
+	double temp;
+	int i, j, k;
+
+	double *pha, *phb, *phc;
+	
+
+		
+    pha = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phb = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phc = (double *)malloc((m_ar * m_ar) * sizeof(double));
+
+	for(i=0; i<m_ar; i++)
+		for(j=0; j<m_ar; j++)
+			pha[i*m_ar + j] = (double)1.0;
+
+
+
+	for(i=0; i<m_br; i++)
+		for(j=0; j<m_br; j++)
+			phb[i*m_br + j] = (double)(i+1);
+
+
+
+    Time1 = clock();
+
+	for(i=0; i<m_ar; i++)
+	{
+		for( j=0; j<m_ar; j++)
+		{	//temp = 0;
+			for( k=0; k<m_br; k++)
+			{	
+				//temp = 0;
+				phc[i*m_ar+k] += pha[i*m_ar+k] * phb[j*m_br+k];
+				//phc[i*m_ar+k]+=temp;
+				//printf("%f ", temp);
+			}
+			//printf("%f ", temp);
+			//printf("\n");
+			//phc[i*m_ar+j]=temp;
+
+		}
+		printf("\n");
+	}
+
+
+    Time2 = clock();
+	sprintf(st, "Time: %3.3f seconds\n", (double)(Time2 - Time1) / CLOCKS_PER_SEC);
+	cout << st;
+
+	// display 10 elements of the result matrix tto verify correctness
+	cout << "Result matrix: " << endl;
+	for(i=0; i<1; i++)
+	{	for(j=0; j<min(10,m_br); j++)
+			cout << phc[j] << " ";
+	}
+	cout << endl;
+
+    free(pha);
+    free(phb);
+    free(phc);
+	
+	
+    
+}
+
+// add code here for block x block matriz multiplication
+void OnMultBlock(int m_ar, int m_br, int bkSize)
+{
+    
+    
+}
+
+
+int main (int argc, char *argv[])
+{
+
+	char c;
+	int lin, col, blockSize;
+	int op;
+	
+	op=1;
+	do {
+		cout << endl << "1. Multiplication" << endl;
+		cout << "2. Line Multiplication" << endl;
+		cout << "3. Block Multiplication" << endl;
+		cout << "Selection?: ";
+		cin >>op;
+		if (op == 0)
+			break;
+		printf("Dimensions: lins=cols ? ");
+   		cin >> lin;
+   		col = lin;
+
+		switch (op){
+			case 1: 
+				OnMult(lin, col);
+				break;
+			case 2:
+				OnMultLine(lin, col);  
+				break;
+			case 3:
+				cout << "Block Size? ";
+				cin >> blockSize;
+				OnMultBlock(lin, col, blockSize);  
+				break;
+
+		}
+
+	}while (op != 0);
+
+}
