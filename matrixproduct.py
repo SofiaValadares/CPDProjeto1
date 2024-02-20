53eef239c032e426310240a928504be03f337015
SofiaValadares <svc3@cesar.school>
2024-02-20 09:32:10 +0000

    python sem papi

--- matrixproduct.py
+++ matrixproduct.py
@@ -11,6 +11,7 @@ def OnMult(m_ar, m_br):
 
     Time1 = time.time()
 
+
     for i in range(m_ar):
         for j in range(m_br):
             temp = 0
@@ -20,6 +21,7 @@ def OnMult(m_ar, m_br):
 
             phc[i*m_ar + j] = temp
 
+
     Time2 = time.time()
     #print(f"Time: {Time2 - Time1} seconds")
     print(f"Time: {((Time2 - Time1)):.3f} seconds\n")
@@ -28,11 +30,41 @@ def OnMult(m_ar, m_br):
     print("Result matrix: ")
     for i in range(1):
         for j in range(min(10, m_br)):
-            print(phc[j], end=" ")
+            print("{:.0f}".format(phc[j]), end=" ")
         print()
 
-#def OnMultLine(m_ar, m_br):
-        
+
+def OnMultLine(m_ar, m_br):
+    pha = [1.0] * (m_ar * m_ar)
+    phb = [0.0] * (m_br * m_br)
+    phc = [0.0] * (m_ar * m_ar)
+
+    for i in range(m_br):
+        for j in range(m_br):
+            phb[i*m_br + j] = i + 1.0
+
+    Time1 = time.time()
+    
+
+    for i in range(m_ar):
+        for j in range(m_ar):
+            for k in range(m_br):
+                phc[i*m_ar + k] += pha[i*m_ar + k] * phb[j*m_ar + k]
+
+
+    
+    Time2 = time.time()
+    #print(f"Time: {Time2 - Time1} seconds")
+    print(f"Time: {((Time2 - Time1)):.3f} seconds\n")
+
+
+    print("Result matrix: ")
+    for i in range(1):
+        for j in range(min(10, m_br)):
+            print("{:.0f}".format(phc[j]), end=" ")
+        print()
+
+
 def OnMultBlock(m_ar, m_br, bkSize):
     pha = [1.0] * (m_ar * m_ar)
     phb = [0.0] * (m_br * m_br)
@@ -44,6 +76,7 @@ def OnMultBlock(m_ar, m_br, bkSize):
 
     Time1 = time.time()
 
+
     for ib in range(0, m_ar, bkSize):
         for jb in range(0, m_br, bkSize):
             for kb in range(0, m_ar, bkSize):
@@ -52,6 +85,7 @@ def OnMultBlock(m_ar, m_br, bkSize):
                         for k in range(kb, min(kb+bkSize, m_ar), 1):
                             phc[i*m_ar + j] += pha[i*m_ar + k] * phb[k*m_br + j]
     
+
     Time2 = time.time()
     #print(f"Time: {Time2 - Time1} seconds")
     print(f"Time: {((Time2 - Time1)):.3f} seconds\n")
@@ -60,7 +94,7 @@ def OnMultBlock(m_ar, m_br, bkSize):
     print("Result matrix: ")
     for i in range(1):
         for j in range(min(10, m_br)):
-            print(phc[j], end=" ")
+            print("{:.0f}".format(phc[j]), end=" ")
         print()
         
         
@@ -83,7 +117,7 @@ while True:
     if op == 1:
         OnMult(lin, col)
     elif op == 2:
-        print("OnMultLine")
+        OnMultLine(lin, col)
     elif op == 3:
         blockSize = int(input("Block Size? "))
         OnMultBlock(lin, col, blockSize)
